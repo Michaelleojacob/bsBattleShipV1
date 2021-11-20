e6b4f3d1dabd67f152e65e7f707a60f2100c94b5
Michael <michaelleojacob@gmail.com>
2021-11-20 10:50:32 -0800

    ship length works. Tested the individual ships, carrier,bs,destroyer,sub,patrol. Everything seems to be working. Added .getStatus and added .getStatus prop/method to the makeShip(2) tests so that they pass.

--- src/shipFactory/shipFac.test.js
+++ src/shipFactory/shipFac.test.js
@@ -1,4 +1,6 @@
-import makeShip from './shipFactory';
+import makeShip, { shipObj } from './shipFactory';
+
+const { carrier, battleship, destroyer, submarine, patrol } = shipObj;
 
 //* testing constructor
 
@@ -7,4 +9,28 @@ test('makeShip() param should only be number', () => {
 });
 test('makeShip() param should only be number', () => {
   expect(makeShip('4')).toBe('error');
+});
+
+//* testing makeship makes an array based on the number in the param
+
+test('makeShip(4) should return an array', () => {
+  expect(makeShip(4).getStatus()).toStrictEqual([1, 2, 3, 4]);
+});
+test('makeShip(2) should return an array', () => {
+  expect(makeShip(2).getStatus()).toStrictEqual([1, 2]);
+});
+test('carrier should be [1,2,3,4,5]', () => {
+  expect(carrier.getStatus()).toStrictEqual([1, 2, 3, 4, 5]);
+});
+test('battleship should be [1,2,3,4]', () => {
+  expect(battleship.getStatus()).toStrictEqual([1, 2, 3, 4]);
+});
+test('destroyer should be [1,2,3]', () => {
+  expect(destroyer.getStatus()).toStrictEqual([1, 2, 3]);
+});
+test('submarine should be [1,2,3]', () => {
+  expect(submarine.getStatus()).toStrictEqual([1, 2, 3]);
+});
+test('patrol should be [1,2]', () => {
+  expect(patrol.getStatus()).toStrictEqual([1, 2]);
 });
--- src/shipFactory/shipFactory.js
+++ src/shipFactory/shipFactory.js
@@ -1,5 +1,21 @@
 export default function makeShip(leng) {
   if (typeof leng !== 'number') return 'error';
   const ship = Array.from(Array(leng).keys()).map((i) => i + 1);
-  return ship;
+
+  const getStatus = () => {
+    console.log(ship);
+    return ship;
+  };
+
+  return { ship, getStatus };
 }
+
+const shipObj = {
+  carrier: makeShip(5),
+  battleship: makeShip(4),
+  destroyer: makeShip(3),
+  submarine: makeShip(3),
+  patrol: makeShip(2),
+};
+
+export { shipObj };
