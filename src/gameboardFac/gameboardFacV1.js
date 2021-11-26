ad3c1215def8dafb593e89479a54235384bc18fa
Michael <michaelleojacob@gmail.com>
2021-11-26 11:50:29 -0800

    Finished console.log testing in script.js, moving to gameBoardFacV1 to jest test it.
    After breaking all tests, I threw Gameboard() in script to attempt to work out why some tests were giving me very odd results. After troubleshooting in script.js I moved the logic back to gameboardFavV1 for further jest testing. Time to start jest tests from basically the beginning again.

--- src/gameboardFac/gameboardFacV1.js
+++ src/gameboardFac/gameboardFacV1.js
@@ -1,17 +1,46 @@
 import boardObj from './boardObj';
 
+function checkSize(size, coordsLength) {
+  const isMatchingSize = size === coordsLength;
+  return isMatchingSize;
+}
+
+function checkCellIsEmpty(board, coordinates) {
+  const areCellsEmpty = coordinates.every((coord) => board[coord] === 'empty');
+  return areCellsEmpty;
+}
+
+function checkCellExists(board, coordinates) {
+  const doCellsExist = coordinates.every((coord) =>
+    Object.prototype.hasOwnProperty.call(board, coord)
+  );
+  return doCellsExist;
+}
+
+const runAllChecks = (gameB, shipSize, coordinates) => {
+  const check1 = checkSize(shipSize, coordinates.length);
+  const check2 = checkCellIsEmpty(gameB, coordinates);
+  const check3 = checkCellExists(gameB, coordinates);
+  const allChecks = check1 && check2 && check3;
+  return allChecks;
+};
+
 function Gameboard() {
   const board = { ...boardObj };
 
-  const placeShip = ({ name, size }, coords) => {
-    if (size !== coords.length) return 'error mismatched ship.size and coords.length';
+  function placeShip({ name, size }, coords) {
+    const foo = runAllChecks(board, size, coords);
 
-    coords.forEach((coord) => {
-      board[coord] = name;
-    });
-    console.log({ ...board });
+    if (!foo) return 'Error: one or more checks failed';
+
+    if (foo) {
+      coords.forEach((coord) => {
+        board[coord] = name;
+      });
+    }
+
     return { ...board };
-  };
+  }
 
   return {
     get board() {
@@ -21,4 +50,4 @@ function Gameboard() {
   };
 }
 
-export default Gameboard;
+export default Gameboard();
--- src/gameboardFac/gameboardFacV1.test.js
+++ src/gameboardFac/gameboardFacV1.test.js
@@ -7,11 +7,11 @@ test('mockBoard.bord() returns an object', () => {
   expect(typeof mockBoard.board).toBe('object');
 });
 
-test('mockBoard.board.length is 100', () => {
+xtest('mockBoard.board.length is 100', () => {
   expect(Object.keys(mockBoard.board).length).toBe(100);
 });
 
-test('mockBoard.board all values are "empty" on creation', () => {
+xtest('mockBoard.board all values are "empty" on creation', () => {
   expect(Object.values(mockBoard.board).every((item) => item === 'empty'));
 });
 
@@ -19,40 +19,40 @@ const mBoard = Gameboard();
 const carrier = Ship('carrier', 1);
 mBoard.placeShip(carrier, ['A0']);
 
-test('mBoard.placeShip changes the correct object key value pairs', () => {
+xtest('mBoard.placeShip changes the correct object key value pairs', () => {
   console.log(mBoard.board);
   expect(mBoard.board).toHaveProperty('A0', 'carrier');
 });
-test('mBoard.board .every === empty should be false', () => {
+xtest('mBoard.board .every === empty should be false', () => {
   expect(Object.values(mBoard.board).every((item) => item === 'empty')).toBe(false);
 });
 
 const b1 = Gameboard();
 const s1 = Ship('s1', 2);
-test('b1.placeShip(s1, [A0]) should be mismatched size and coords', () => {
+xtest('b1.placeShip(s1, [A0]) should be mismatched size and coords', () => {
   expect(b1.placeShip(s1, ['A0'])).toBe('error mismatched ship.size and coords.length');
 });
-test('b1 should still be all empty due to the above placeShip being error"ed out', () => {
+xtest('b1 should still be all empty due to the above placeShip being error"ed out', () => {
   expect(Object.values(b1.board).every((item) => item === 'empty')).toBe(true);
 });
 
 const b2 = Gameboard();
 const s2 = Ship('s2', 1);
-test('b2.placeShip(s1, [P2]) to give an error', () => {
+xtest('b2.placeShip(s1, [P2]) to give an error', () => {
   expect(b2.placeShip(s2, ['P2'])).toBe('error one or more coordinates are not valid spaces');
 });
-test('b2 should still be all empty due to the above placeShip being error"ed out', () => {
+xtest('b2 should still be all empty due to the above placeShip being error"ed out', () => {
   expect(Object.values(b2.board).every((item) => item === 'empty')).toBe(true);
 });
 
 const b3 = Gameboard();
 const s3 = Ship('s3', 5);
-test('b3 should error out if I place even 1/5 coordinates are invalid', () => {
+xtest('b3 should error out if I place even 1/5 coordinates are invalid', () => {
   expect(b3.placeShip(s3, ['A0', 'A1', 'A2', 'A3', 'M12'])).toBe(
     'error either coord does not exist, or space is not empty'
   );
 });
-test('b3.board should still be empty', () => {
+xtest('b3.board should still be empty', () => {
   expect(Object.values(b3.board).every((item) => item === 'empty')).toBe(true);
 });
 
